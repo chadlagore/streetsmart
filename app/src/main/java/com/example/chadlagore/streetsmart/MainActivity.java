cbf582dd8206a3a308d1a4e38050c4ac08f5d10f
chadlagore <chad.d.lagore@gmail.com>
2017-03-13 15:12:23 -0700

    travis and map timer

--- app/src/main/java/com/example/chadlagore/streetsmart/MainActivity.java
+++ app/src/main/java/com/example/chadlagore/streetsmart/MainActivity.java
@@ -1,32 +1,87 @@
 package com.example.chadlagore.streetsmart;
 
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    SupportMapFragment mapFragment;
+    MapFragment mapFragment;
+    GoogleMap googleMap;
+    Timer updateMapTimer;
+
+    int updateMapTime = 5000; // ms
+    int updateMapDelay = 3000; // ms
+    double markerLat = 49.000;
+    double markerLon = 122.000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapFragment = getMapFragement();
+        mapFragment.getMapAsync(this);
     }
 
+    /** Initializes the map updater. */
+    private void initMapUpdateTimer() {
+        Timer updateMapTimer = new Timer();
+        UpdateMapTask my_task = new UpdateMapTask();
+        updateMapTimer.schedule(my_task, updateMapDelay, updateMapTime);
+    }
+
+    /** Asyncronously gets access to map. */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap = googleMap;
+        initMapUpdateTimer();
+    }
+
+    /** Class performs async updates to map. */
+    public class UpdateMapTask extends TimerTask {
+        public void run() {
+            markerLat += 1;
+            markerLon += 1;
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    updateMapMarkers();
+                }
+            });
+        }
+    }
+
+    private void updateMapMarkers() {
+        if (mapFragment == null) {
+            Log.i("gmaps_timer", "map null");
+        }
+
+        Log.i("gmaps_timer", "updating map markers");
+    }
+
+    /** Handles satellite button click. */
     public void onSatelliteClick(View view) {
         FragmentManager getSupportFragmentManager ;
         MapFragment fragment = (MapFragment)getSupportFragmentManager()
@@ -34,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
         fragment.changeMapType(fragment.MAP_TYPES[GoogleMap.MAP_TYPE_SATELLITE]);
     }
 
+    /** Handles normal button click. */
     public void onNormalClick(View view) {
         FragmentManager getSupportFragmentManager ;
         MapFragment fragment = (MapFragment)getSupportFragmentManager()
@@ -41,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
         fragment.changeMapType(fragment.MAP_TYPES[GoogleMap.MAP_TYPE_NORMAL]);
     }
 
+    /** Handles hybrid button click. */
     public void onHybridClick(View view) {
         FragmentManager getSupportFragmentManager ;
         MapFragment fragment = (MapFragment)getSupportFragmentManager()
@@ -48,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
         fragment.changeMapType(fragment.MAP_TYPES[GoogleMap.MAP_TYPE_HYBRID]);
     }
 
+    /** Handles Terrain button click. */
     public void onTerrainClick(View view) {
         FragmentManager getSupportFragmentManager ;
         MapFragment fragment = (MapFragment)getSupportFragmentManager()
@@ -55,10 +113,16 @@ public class MainActivity extends AppCompatActivity {
         fragment.changeMapType(fragment.MAP_TYPES[GoogleMap.MAP_TYPE_TERRAIN]);
     }
 
+    /** Handles None button click. */
     public void onNoneClick(View view) {
         FragmentManager getSupportFragmentManager ;
         MapFragment fragment = (MapFragment)getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         fragment.changeMapType(fragment.MAP_TYPES[GoogleMap.MAP_TYPE_NONE]);
+    }
+
+    /** Returns map fragment. */
+    public MapFragment getMapFragement() {
+        return (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
     }
 }
