f007e7035321586a6a9b554aaeaf55ac5bd79fd7
Bruno Bachmann <bfbachmann@gmail.com>
2017-03-14 17:55:32 -0700

    Fix bugs in BluetoothConnection and add device discovery We can't see the descovered devices for some reason... but they're there

--- app/src/main/java/com/example/chadlagore/streetsmart/BluetoothConnectionActivity.java
+++ app/src/main/java/com/example/chadlagore/streetsmart/BluetoothConnectionActivity.java
@@ -15,8 +15,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 import static com.example.chadlagore.streetsmart.R.id.bluetooth_connection_toolbar;
@@ -26,30 +29,14 @@ import static com.example.chadlagore.streetsmart.R.id.bluetooth_connection_toolb
  */
 
 public class BluetoothConnectionActivity extends AppCompatActivity {
-    private final static int REQUEST_ENABLE_BT = 1;
-    ProgressBar spinnyWheel;
-    ViewGroup thisViewGroup;
-    Context thisContext;
-    BluetoothAdapter bluetoothAdapter;
 
-    /**
-     * Create a BroadcastReceiver for Bluetooth connections.
-     */
-    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                /*
-                 * Bluetooth discovery has found a device. Get the BluetoothDevice
-                 * object and its info from the Intent.
-                 */
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress();
-                // TODO: implement this
-            }
-        }
-    };
+    /* Some variables we will need access to throughout this activity */
+    private final static int REQUEST_ENABLE_BT = 1;
+    private ProgressBar spinnyWheel;
+    private LinearLayout deviceListLayout;
+    private BluetoothAdapter bluetoothAdapter;
+    private ArrayList<BluetoothDevice> deviceList;
+    private BroadcastReceiver broadcastReceiver;
 
 
     /**
@@ -60,21 +47,29 @@ public class BluetoothConnectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_connection);
-        spinnyWheel = (ProgressBar) findViewById(R.id.progressWheel);
-        thisViewGroup = (ViewGroup) findViewById(R.id.btConnectLinearLayout);
-        thisContext = getApplicationContext();
+        spinnyWheel = (ProgressBar) findViewById(R.id.progress_wheel);
+        deviceListLayout = (LinearLayout) findViewById(R.id.bt_linear_layout);
+        deviceList = new ArrayList<>();
 
         /* Add the toolbar so we have access to the "Back" button */
         Toolbar myChildToolbar = (Toolbar) findViewById(R.id.bluetooth_connection_toolbar);
         setSupportActionBar(myChildToolbar);
-
-        /* Expand the toolbar at the top of the screen */
         Toolbar appToolbar = (Toolbar) findViewById(bluetooth_connection_toolbar);
         setSupportActionBar(appToolbar);
-
-        /* Enable up button on support ActionBar corresponding to this toolbar */
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+         /* Create a BroadcastReceiver for Bluetooth connections. */
+        broadcastReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                /* Bluetooth discovery has found a device. Add it to the device list */
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    addToDeviceList(device);
+                }
+            }
+        };
 
         /* Create a Bluetooth Adapter */
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -85,34 +80,18 @@ public class BluetoothConnectionActivity extends AppCompatActivity {
              */
             showBluetoothDialog("This device does not appear to have Bluetooth capabilities which " +
                 "are required for this action.", "Bluetooth Unavailable");
-        } else if (!bluetoothAdapter.isEnabled()) {
-            /* Bluetooth is available but not enabled, request enable */
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-
-        if (bluetoothAdapter != null) {
-            /* Make the progress wheel visible while we search for devices */
-            spinnyWheel.setVisibility(View.VISIBLE);
-
-            /* Register for broadcasts when a Bluetooth device is discovered. */
-            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(broadcastReceiver, filter);
-        }
-    }
-
-
-    /**
-     * This method is called when the object of this class is destroyed
-     */
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        /* Don't forget to unregister the ACTION_FOUND receiver. */
-        try {
-            unregisterReceiver(broadcastReceiver);
-        } catch (Exception e) {
-            // TODO: do something
+        } else {
+            if (!bluetoothAdapter.isEnabled()) {
+                /*
+                 * Bluetooth is available but not enabled, request enable. The user's response to
+                 * this is handled in onActivityResult()
+                 */
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            } else {
+                /* Bluetooth is enabled, check for devices */
+                checkAvailableDevices();
+            }
         }
     }
 
@@ -125,25 +104,11 @@ public class BluetoothConnectionActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {
             /* The user gave us Bluetooth permissions! YAY! */
-
-            /* Search for paired Bluetooth devices */
-            Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-
-            if (pairedDevices.isEmpty()) {
-                /* There are no paired devices so we have to scan for available ones */
-                boolean searchBegan = bluetoothAdapter.startDiscovery();
-                if (!searchBegan) {
-                    /* Device search failed for some reason */
-                    showBluetoothDialog("Bluetooth Device Discovery failed.", "OOPS!");
-                } else {
-                    /* Set up a view so the user can see available devices */
-                    showBluetoothDialog("This feature has not yet been implemented.", "OOPS!");
-                }
-            }
+           checkAvailableDevices();
         } else {
             /* The user would not grant us Bluetooth permissions */
             showBluetoothDialog("Sorry, you must grant Bluetooth permissions to connect to " +
-                            "external devices.", "Permissions Required.");
+                            "external devices.", "Permissions Required");
         }
     }
 
@@ -172,5 +137,82 @@ public class BluetoothConnectionActivity extends AppCompatActivity {
             }
         });
         dialog.show();
+    }
+
+
+
+    /**
+     * This method is called when the object of this class is destroyed
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            unregisterReceiver(broadcastReceiver);
+        } catch (IllegalArgumentException e) {
+            /* We are trying to unregister an already unregistered receiver */
+        }
+
+    }
+
+    private void checkAvailableDevices() {
+         /* Search for paired Bluetooth devices */
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.isEmpty()) {
+            /* There are no paired devices so we have to scan for available ones */
+            spinnyWheel.setVisibility(View.VISIBLE);
+            setDeviceListVisibility(TextView.VISIBLE);
+            /*
+             * Register for broadcasts when a Bluetooth device is discovered.
+             * broadcastReceiver.onReceive() should be called every time a device is found.
+             */
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(broadcastReceiver, filter);
+            boolean searchBegan = bluetoothAdapter.startDiscovery();
+            if (!searchBegan) {
+                /* Device search failed for some reason, return to MainActivity */
+                showBluetoothDialog("Bluetooth Device Discovery failed.", "OOPS!");
+            }
+        } else {
+            /* We are already paired TODO: actually implement this */
+            showBluetoothDialog("Nice! We're already paired with something.", "Already Paired!");
+        }
+    }
+
+
+    /**
+     * Add a device to the list of available devices and display the new device
+     * @param device
+     */
+    private void addToDeviceList(BluetoothDevice device) {
+        runOnUiThread(new DeviceListPopulator(device));
+    }
+
+
+    /**
+     * Will make the deviceList in activity_bluetooth_connection visible
+     * This device list will be updated with new devices every time one is detected
+     * @param visibileOrNot one of TextView.VISIBLE and TextView.INVISIBLE
+     */
+    private void setDeviceListVisibility(int visibileOrNot) {
+        ((ViewGroup)deviceListLayout).setVisibility(visibileOrNot);
+    }
+
+
+    /* For dynamically populating the list of available Bluetooth devices */
+    public class DeviceListPopulator implements Runnable {
+        BluetoothDevice device;
+
+        DeviceListPopulator(BluetoothDevice device) {
+            this.device = device;
+        }
+
+        public void run() {
+            TextView deviceView = new TextView(getApplicationContext());
+            deviceView.setText(device.getName());
+            deviceListLayout.addView(deviceView);
+            deviceList.add(device);
+        }
     }
 }
