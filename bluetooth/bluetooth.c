dcbf4bcc48f001dc915fe08a95836ec12804042f
deadbass <roryjf@outlook.com>
2017-03-16 12:58:18 -0700

    updated bluetooth files

---
+++ bluetooth/bluetooth.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "bluetooth.h"
+
+
+void init_bluetooth(void){
+	BLUETOOTH_CONTROL = 0x15;
+	BLUETOOTH_BAUD = 0x01; //datasheet recommends 115k baud rate
+}
+
+void sendchar_bluetooth(char c){
+    while(1) {
+			if (BLUETOOTH_STATUS & BLUETOOTH_TX_MASK) {
+				BLUETOOTH_TXDATA = c;
+				break;
+			}
+		}
+}
+
+void test_get_char(void){
+	return BLUETOOTH_STATUS & BLUETOOTH_RX_MASK;
+}
+
+char get_char(void){
+	if (BLUETOOTH_STATUS & BLUETOOTH_RX_MASK) {
+		return BLUETOOTH_RXDATA;
+	}
+	else {
+		return '-';
+	}
+}
+
+char polling_char(void) {
+	while (!(BLUETOOTH_STATUS & BLUETOOTH_RX_MASK));
+
+	return BLUETOOTH_RXDATA;
+}
+
+void receive(char incoming[]) {
+
+	int i = 0;
+	length = strlen(incoming);
+
+	while (i < length) {
+		if ((incoming[i] = get_char()) != '-') {
+
+			printf("%c", incoming[i]);
+
+			if (incoming[i] == '\0' || incoming[i] == '?') {
+				printf("done");
+				break;
+			}
+			usleep(100000);
+			i++
+		}
+	}
+	//debug print
+	incoming[i] = '\0'
+}
+
+void sendstring_bluetooth(char str[]){
+
+  int i;
+	int length = strlen(str);
+
+    for (i = 0; i < length && str[i] != '\0'; i++) {
+    	usleep(100000); //100ms wait
+			sendchar_bluetooth(str[i]);
+	}
+}
+
+void command_mode(void){
+  printf("Entering Command Mode...\n");
+	usleep(100000); // 1s wait
+	sendstring_bluetooth("$$$");
+	usleep(100000);
+
+//	wait_for_read();
+//	char O = get_char();
+//	printf("%c", O);
+//	wait_for_read();
+//	char K = get_char();
+//	printf("%c\n", K);
+}
+
+void data_mode(void) {
+  printf("Entering Data Mode...\n");
+	usleep(100000); // 1s wait
+	sendstring_bluetooth("---\r\n");
+	usleep(100000);
+}
+
+void reset_bluetooth(void) {
+	command_mode();
+	sendstring_bluetooth("SF,");
+	sendstring_bluetooth("1\r\n");
+	data_mode();
+}
+
+void set_name(char name[]) {
+	command_mode();
+	sendstring_bluetooth("SN,");
+	sendstring_bluetooth(name);
+	sendstring_bluetooth("\r\n");
+	data_mode();
+}
+
+void set_pw(char pw[]) {
+	command_mode();
+	sendstring_bluetooth("SP,");
+	sendstring_bluetooth(pw);
+	sendstring_bluetooth("\r\n");
+	data_mode();
+}
+
+void transmit_string(char str[]) {
+	int i;
+	int length = strlen(str);
+
+	for (i = 0; i < length; i++){
+		sendchar_bluetooth(str[i]);
+	}
+	sendchar_bluetooth("\r");
+	sendchar_bluetooth("\n");
+}
+
+void test_bluetooth(void) {
+    printf("Testing Bluetooth...\n");
+    printf("Connection established...\n");
+
+    const char btname[] = "bluetooth_is_suffering";
+    const char password[] = "morning";
+
+    set_name(btname);
+    set_pw(password);
+
+    printf("Bluetooth successfully updated!\n");
+}
---
+++ bluetooth/bluetooth.h
@@ -0,0 +1,29 @@
+#ifndef _BLUETOOTH_H_
+#define _BLUETOOTH_H_
+
+/* Define bluetooth control registers */
+#define BLUETOOTH_STATUS   (*(volatile unsigned char *)(0x84000220))
+#define BLUETOOTH_CONTROL  (*(volatile unsigned char *)(0x84000220))
+#define BLUETOOTH_TXDATA   (*(volatile unsigned char *)(0x84000222))
+#define BLUETOOTH_RXDATA   (*(volatile unsigned char *)(0x84000222))
+#define BLUETOOTH_BAUD     (*(volatile unsigned char *)(0x84000224))
+
+#define BLUETOOTH_TX_MASK 0x02
+#define BLUETOOTH_RX_MASK 0x01
+
+void init_bluetooth(void);
+void sendchar_bluetooth(char c);
+void test_get_char(void);
+char get_char(void);
+char polling_char(void);
+void receive(char incoming[]);
+void sendstring_bluetooth(char str[]);
+void command_mode(void);
+void data_mode(void);
+
+void reset_bluetooth(void);
+void set_name(char name[]);
+void set_pw(char pw[]);
+void test_bluetooth();
+
+#endif /* _BLUETOOTH_H_ */
